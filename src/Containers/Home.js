c7474a983c4ce408a5c81b9a4022d4ff9803d3e1
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-04 01:27:40 +0530

    Update index paths

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,12 +1,14 @@
 /*eslint-disable*/
 
 import { useEffect } from 'react';
+import { connect } from 'react-redux';
 import { getcourses, getcoursesingle, signin, signup, getfavs } from '../API/apicalls';
 import Navbar from '../Components/Navbar'
+import Homecourses from '../Components/Homecourses'
 
+function Home(props) {
 
-
-export default function Home() {
+  const { courses } = props;
 
   useEffect(() => {
     // getfavs();
@@ -14,9 +16,48 @@ export default function Home() {
 
   return (
     <>
-        <Navbar />
-
-      <h1>This page has all the courses</h1>
+      <Navbar />
+            <div className="d-flex flex-wrap container p-0 m-auto justify-content-center">
+       {courses.length === 0 ? <h5>Loading... </h5>
+          : 
+        // courses[0].map((x)=>(<h1>{x.name}</h1>))}
+          courses[0].map((x) => (
+            <div key={x.id} className="border d-flex align-items-center justify-content-center m-2 py-4">
+              <Homecourses x={x} />
+            </div>
+          ))}
+      {/* {console.log(courses)} */}
+      </div>
     </>
   );
 }
+
+
+
+function mapStateToProps(state) {
+  const { courses } = state.coursersreducer;
+  // const { filter } = state.filterReducer;
+  // const { namefilter } = state.namefilterReducer;
+  // return ({ teams, filter, namefilter });
+  return ({ courses })
+}
+
+// const mapDispatchToProps = (dispatch) => ({
+//   filtercat: (category) => dispatch(changeFilter(category)),
+//   namefiltercat: (category) => dispatch(namechangefilter(category)),
+
+// });
+
+// Homepage.propTypes = {
+//   teams: PropTypes.arrayOf(PropTypes.object),
+//   filtercat: PropTypes.func.isRequired,
+//   namefiltercat: PropTypes.func.isRequired,
+//   filter: PropTypes.string.isRequired,
+//   namefilter: PropTypes.string.isRequired,
+// };
+
+// Homepage.defaultProps = {
+//   teams: [],
+// };
+
+export default connect(mapStateToProps, null)(Home);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 import Routes from './Routes/routes';
+import rootReducer from './Reducers/rootreducer';
 import { BrowserRouter } from 'react-router-dom';
+import { getcourses } from './API/apicalls'
 
 const inistate = {
   favs: [],
@@ -16,16 +19,17 @@ const inistate = {
   username: ''
 }
 
-// const store = createStore(reducer, {reducer: inistate} )
+const store = createStore(rootReducer, {coursersreducer: inistate},  applyMiddleware(thunk) )
+
+store.dispatch(getcourses);
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store = { store }> */}
+    <Provider store = { store }>
       <BrowserRouter>
-
         <Routes />
       </BrowserRouter>
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
