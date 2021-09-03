6909422ca7ecd44a60b81d9fe8f39c3642541b37
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-04 01:26:17 +0530

    Update login container with hook

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -1,22 +1,24 @@
 /*eslint-disable*/
 
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import LoginForm from '../Components/Loginform';
 import { signin } from '../API/apicalls';
 
-export default function Login(props) {
+export default function Login() {
+  const history = useHistory();
+  
   const submitChange = (event) => {
     event.preventDefault();
     const params = { 'email': event.target.Email1.value, password: event.target.Password1.value };
-    signin(params);
+    signin(params,history);
     // if (event.target.id === 'exampleselectInput1' && event.target.value !== '') {
     //   setState1((prevstate) => ({ ...prevstate, Category: event.target.value }));
     // }
     // addnewbook(state);
     // setState1(() => ({ Title: '', Category: '' }));
   };
-  console.log(props.history);
+
   return (
 
     <>
