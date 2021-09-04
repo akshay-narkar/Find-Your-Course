cc2fe33e40ba5c646983433ddc2b353aef70f9b1
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-05 03:10:46 +0530

    Add course component

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -5,6 +5,7 @@ import Login from '../Containers/Login';
 import Signup from '../Containers/Signup';
 import Signout from '../Components/Signoutmessage';
 import Course from '../Containers/Coursepage';
+import Favs from '../Containers/Fav';
 
 export default function Routes() {
   return (
@@ -17,7 +18,7 @@ export default function Routes() {
         <Route path="/login" component={Login} />
         <Route path="/logout" component={Signout} />
         <Route path="/course/:id" component={Course} />
-        {/* <Route path = ":user_id/favs/:id" component = {Favs}></Route> */}
+        <Route path="/users/:user_id/favs" component={Favs} />
         <Route component={Error} />
       </Switch>
     </>
