3faddca9bb048460998f15f9d8af892c983ec7fc
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-06 02:32:08 +0530

    Add reducer tests

---
+++ src/tests/Reducer Tests/reducer.test.js
@@ -0,0 +1,91 @@
+import {
+  ALLCOURSES, FAVCOURSES, ERROR,
+  USERDEETS, FAVCOURSEADD,
+} from '../../Actions/index';
+
+import coursersreducer from '../../Reducers/coursereducer';
+import favsreducer from '../../Reducers/favsreducer';
+import usersreducer from '../../Reducers/userreducer';
+import errorsreducer from '../../Reducers/errorreducer';
+
+const state = { courses: [] };
+const favs = { favs: [] };
+const favlist = { favlist: [] };
+const user = {
+  userid: null,
+  username: null,
+};
+
+describe('Courses Reducer', () => {
+  describe('INITIAL_DUMMY_STATE', () => {
+    test('Pass default state', () => {
+      const action = { type: 'dummy_action' };
+      const initialState = { courses: 0 };
+
+      expect(coursersreducer(initialState, action)).toEqual(initialState);
+      expect(coursersreducer(undefined, action)).not.toEqual(initialState);
+    });
+  });
+
+  describe('USE THE CORRECT GET COURSES ACTION', () => {
+    test('returns the correct state', () => {
+      const action = { type: ALLCOURSES, courses: 1 };
+      const expectedState = { courses: [1] };
+      const unexpectedState = { courses: [2] };
+      expect(coursersreducer(state, action)).toEqual(expectedState);
+      expect(coursersreducer(state, action)).not.toEqual(unexpectedState);
+    });
+  });
+});
+
+describe('Favs Reducer', () => {
+  describe('USE THE ALL FAVS ACTION', () => {
+    test('returns the correct state', () => {
+      const action = { type: FAVCOURSES, favs: 1 };
+      const expectedState = { favs: [1] };
+      const unexpectedState = { favs: [2] };
+      expect(favsreducer(favs, action)).toEqual(expectedState);
+      expect(favsreducer(favs, action)).not.toEqual(unexpectedState);
+    });
+  });
+
+  describe('USE THE ADD FAV ACTION', () => {
+    test('returns the correct state', () => {
+      const action = { type: FAVCOURSEADD, fav: 1 };
+      const expectedState = { favlist: [1] };
+      const unexpectedState = { favlist: [2] };
+      expect(favsreducer(favlist, action)).toEqual(expectedState);
+      expect(favsreducer(favlist, action)).not.toEqual(unexpectedState);
+    });
+  });
+});
+
+describe('Users Reducer', () => {
+  describe('USE THE CORRECT GET USERS ACTION', () => {
+    test('returns the correct state', () => {
+      const action = { type: USERDEETS, user: { userid: 1, username: 'Spain' } };
+      const expectedState = {
+        userid: 1,
+        username: 'Spain',
+      };
+      const unexpectedState = {
+        userid: 2,
+        username: 'ENG',
+      };
+      expect(usersreducer(user, action)).toEqual(expectedState);
+      expect(usersreducer(user, action)).not.toEqual(unexpectedState);
+    });
+  });
+});
+
+describe('Errors Reducer', () => {
+  describe('USE THE CORRECT ERROR ACTION', () => {
+    test('returns the correct state', () => {
+      const action = { type: ERROR, text: 'Error' };
+      const expectedState = { error: 'Error' };
+      const unexpectedState = { error: 'Xyz' };
+      expect(errorsreducer({ error: 'Error' }, action)).toEqual(expectedState);
+      expect(errorsreducer({ error: 'All' }, action)).not.toEqual(unexpectedState);
+    });
+  });
+});
