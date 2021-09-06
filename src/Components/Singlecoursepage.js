83ef3b59df740a2196251dad19dfc4462a5bbfb3
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-07 04:27:40 +0530

    Change fonts of placeholders

--- src/Components/Singlecoursepage.js
+++ src/Components/Singlecoursepage.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import ReactStars from 'react-stars';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import image from '../images/signup.jpg';
 
 function Singlecoursepage(props) {
   const {
@@ -14,7 +13,7 @@ function Singlecoursepage(props) {
   };
 
   const size = {
-    'font-size': '1.4rem',
+    'font-size': '1.3rem',
   };
 
   const color = {
@@ -24,21 +23,24 @@ function Singlecoursepage(props) {
   return (
     <>
       <div className="card singlecardborder shadow-card" id={x.id}>
-        <img src={image} style={imagewidth} className="card-img-top-favs" alt="course" />
+        <img src={x.course_photo} style={imagewidth} className="card-img-top-favs" alt="course" />
         <div className="card-body d-flex justify-content-left">
           <div>
-            <h4 className="card-title fw-bold my-3">
+            <h5 className="card-title fw-normal my-3">
               <span className="fw-bold">Taught By:</span>
+&nbsp;
               {x.teacher_name}
-            </h4>
-            <h5 className="card-text fw-normal">
+            </h5>
+            <h5 className="card-text fw-normal my-3">
               <span className="fw-bold">Domain:</span>
+&nbsp;
               {x.domain}
             </h5>
-            <p className="card-text fw-bold d-flex align-items-center justify-content-left my-2">
+            <h5 className="card-text d-flex align-items-center justify-content-left my-3">
               <span className="fw-bold" style={Object.assign(size, color)}>
                 {' '}
-                <span className="fw-bold">Rating:</span>
+                <span className="fw-normal text-dark">Rating:</span>
+&nbsp;
                 {x.rating}
                 {' '}
               </span>
@@ -49,20 +51,16 @@ function Singlecoursepage(props) {
                 edit={false}
                 color2="#b79c07"
               />
-            </p>
-            <h5 className="card-text fw-normal my-2">
-              <span className="fw-bold">Fees:</span>
+            </h5>
+            <h5 className="card-text fw-normal my-3">
+              <span className="fw-normal">Fees:</span>
 &nbsp;$
               {x.fee}
             </h5>
-            <h5 className="card-text fw-bold my-2">
+            <h5 className="card-text fw-bold my-3">
               About The Course:
-              <span>
-                {' '}
-                {x.details}
-                {' '}
-              </span>
             </h5>
+            <p>{x.details}</p>
 
           </div>
         </div>
@@ -74,7 +72,7 @@ function Singlecoursepage(props) {
           ? (
             <>
               <div className="text-center">
-                <button type="button" className="btn w-100 favsbtn shadow fw-bold">In Favs</button>
+                <button type="button" className="btn w-100 favsbtn shadow fw-bold favssize">In Favs</button>
               </div>
             </>
           )
@@ -82,7 +80,7 @@ function Singlecoursepage(props) {
             <>
               <div className="text-center">
 
-                <button type="button" className="btn w-100 favsbtn shadow fw-bold" onClick={clickhandler}>Add to Favorites</button>
+                <button type="button" className="btn w-100 favsbtn shadow fw-bold favssize" onClick={clickhandler}>Add to Favorites</button>
               </div>
             </>
           )
@@ -90,7 +88,7 @@ function Singlecoursepage(props) {
           <div className="text-center">
 
             <Link to={{ pathname: '/login' }} className="link">
-              <button type="button" className="btn w-100 favsbtn shadow fw-bold">Add to Favorites</button>
+              <button type="button" className="btn w-100 favsbtn shadow fw-bold favssize">Add to Favorites</button>
             </Link>
           </div>
         )}
@@ -106,18 +104,27 @@ Singlecoursepage.propTypes = {
   pageid: PropTypes.string.isRequired,
   clickhandler: PropTypes.func.isRequired,
   x: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    domain: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    details: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-    fee: PropTypes.number.isRequired,
-    teacher_name: PropTypes.string.isRequired,
-  }).isRequired,
+    id: PropTypes.number,
+    domain: PropTypes.string,
+    name: PropTypes.string,
+    details: PropTypes.string,
+    rating: PropTypes.number,
+    fee: PropTypes.number,
+    teacher_name: PropTypes.string,
+    course_photo: PropTypes.string,
+  }),
 };
 
 Singlecoursepage.defaultProps = {
   favlist: [''],
+  x: PropTypes.shape({
+    id: 1,
+    domain: 'Tech',
+    name: 'Prof X',
+    rating: 5,
+    fee: 25,
+    course_photo: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp2698114.jpg&f=1&nofb=1',
+  }),
 };
 
 function mapStateToProps(state) {
