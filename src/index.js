e07fd3f7723d6815500ca18c9ffe2491e60bdb59
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-07 01:13:01 +0530

    Update bootstrap js file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-/*eslint-disable*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.min';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes/routes';
 import rootReducer from './Reducers/rootreducer';
-import { BrowserRouter } from 'react-router-dom';
-import { getcourses } from './API/apicalls'
+import { getcourses } from './API/apicalls';
 
 const inistate = {
   favs: [],
   courses: [],
   username: '',
-  favlist: []
-}
+  favlist: [],
+};
 
-const store = createStore(rootReducer, {coursersreducer: inistate, favsreducer: inistate},  applyMiddleware(thunk) )
+const store = createStore(rootReducer,
+  { coursersreducer: inistate, favsreducer: inistate }, applyMiddleware(thunk));
 
 store.dispatch(getcourses);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store = { store }>
+    <Provider store={store}>
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
@@ -34,7 +33,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
