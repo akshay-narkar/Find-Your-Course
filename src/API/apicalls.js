4207af885015f3b584c293453c2b663273073c1f
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-08 02:31:02 +0530

    Add base url in api calls

--- src/API/apicalls.js
+++ src/API/apicalls.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { allcourse } from '../Actions/index';
 
 const delay = (time) => new Promise((res) => setTimeout(res, time));
-
+const baseURL = 'https://courses-api-microverse.herokuapp.com'
 async function getcourses(dispatch1) {
   try {
-    const response = await axios.get('https://courses-api-microverse.herokuapp.com/api/v1/courses');
+    const response = await axios.get(`${baseURL}/api/v1/courses`);
     dispatch1(allcourse(response.data.data));
   } catch (error) {
     // console.error(error);
@@ -14,7 +14,7 @@ async function getcourses(dispatch1) {
 
 async function signup(signUp, history, errordispatch, userdispatch) {
   try {
-    const response = await axios.post('https://courses-api-microverse.herokuapp.com/api/v1/auth', {
+    const response = await axios.post(`${baseURL}/api/v1/auth`, {
       name: signUp.name,
       email: signUp.email,
       password: signUp.password,
@@ -34,7 +34,7 @@ async function signup(signUp, history, errordispatch, userdispatch) {
 
 async function signin(params, history, errordispatch, userdispatch) {
   try {
-    const response = await axios.post('https://courses-api-microverse.herokuapp.com/api/v1/auth/sign_in', {}, {
+    const response = await axios.post(`${baseURL}/api/v1/auth/sign_in`, {}, {
       headers: params,
     });
 
@@ -53,7 +53,7 @@ async function signin(params, history, errordispatch, userdispatch) {
 async function signout(authParams, history) {
   try {
     sessionStorage.clear();
-    await axios.delete('https://courses-api-microverse.herokuapp.com/api/v1/auth/sign_out', { params: authParams });
+    await axios.delete(`${baseURL}/api/v1/auth/sign_out`, { params: authParams });
     history.push('/login');
   } catch (error) {
     // return error;
@@ -62,7 +62,7 @@ async function signout(authParams, history) {
 
 async function getfavs(userid, dispatch1, authParams, errordispatch) {
   try {
-    const response = await axios.get(`https://courses-api-microverse.herokuapp.com/api/v1/users/${userid}/favs`,
+    const response = await axios.get(`${baseURL}/api/v1/users/${userid}/favs`,
       { params: authParams });
     dispatch1((response.data.data));
   } catch (error) {
@@ -75,7 +75,7 @@ async function getfavs(userid, dispatch1, authParams, errordispatch) {
 
 async function addfavapi(userid, dispatch1, authParams, id, errordispatch) {
   try {
-    const response = await axios.post(`https://courses-api-microverse.herokuapp.com/api/v1/users/${userid}/favs`,
+    const response = await axios.post(`${baseURL}/api/v1/users/${userid}/favs`,
       { course_id: id }, { headers: authParams });
     dispatch1((response.data.data.course_id));
   } catch (error) {
