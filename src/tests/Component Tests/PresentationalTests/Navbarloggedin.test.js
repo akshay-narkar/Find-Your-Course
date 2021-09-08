c94e7e46a1920f576d639a31fd7b8abaf1e5ea64
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-09 03:49:19 +0530

    Update Reducer test and failing snaps

--- src/tests/Component Tests/PresentationalTests/Navbarloggedin.test.js
+++ src/tests/Component Tests/PresentationalTests/Navbarloggedin.test.js
@@ -1,12 +1,10 @@
 /*eslint-disable*/
 import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Navbarloggedin from '../../../Components/Navbarloggedin';
-import store from '../store';
 
 it('UI is unaltered wrt first snapshot', () => {
-  const elem = renderer.create(<BrowserRouter><Provider store={store}><Navbarloggedin /></Provider></BrowserRouter>).toJSON();
+  const elem = renderer.create(<BrowserRouter><Navbarloggedin /></BrowserRouter>).toJSON();
 
   expect(elem).toMatchSnapshot();
 });
