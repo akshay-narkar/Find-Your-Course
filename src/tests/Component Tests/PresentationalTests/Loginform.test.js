214421e406ec518171a695a1a2286a925cefcba8
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-08 23:44:20 +0530

    Update snapshots for presentational comps

--- src/tests/Component Tests/PresentationalTests/Loginform.test.js
+++ src/tests/Component Tests/PresentationalTests/Loginform.test.js
@@ -1,13 +1,15 @@
 /*eslint-disable*/
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Loginform from '../../../Components/Loginform';
+import store from '../store';
 
 const error = 'Random error';
 const clickHandler1 = () => {};
 
 it('UI is unaltered wrt first snapshot', () => {
-  const elem = renderer.create(<BrowserRouter><Loginform error={error} clickHandler={clickHandler1} /></BrowserRouter>).toJSON();
+  const elem = renderer.create(<BrowserRouter><Provider store={store}><Loginform error={error} clickHandler={clickHandler1} /></Provider></BrowserRouter>).toJSON();
 
   expect(elem).toMatchSnapshot();
 });
--- src/tests/Component Tests/PresentationalTests/Signupform.test.js
+++ src/tests/Component Tests/PresentationalTests/Signupform.test.js
@@ -2,11 +2,13 @@
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import Signform from '../../../Components/Signupform';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 const clickHandler1 = () => {};
 
 it('UI is unaltered wrt first snapshot', () => {
-  const elem = renderer.create(<BrowserRouter><Signform clickHandler={clickHandler1} /></BrowserRouter>).toJSON();
+  const elem = renderer.create(<BrowserRouter><Provider store={store}><Signform clickHandler={clickHandler1} /></Provider></BrowserRouter>).toJSON();
 
   expect(elem).toMatchSnapshot();
 });
