1f806c48f9e034a2af411173c674e611a3dcf34e
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-09 03:54:31 +0530

    Remove user reducer

--- src/Reducers/rootreducer.js
+++ src/Reducers/rootreducer.js
@@ -2,10 +2,9 @@ import { combineReducers } from 'redux';
 import coursersreducer from './coursereducer';
 import favsreducer from './favsreducer';
 import errorsreducer from './errorreducer';
-import usersreducer from './userreducer';
 
 const rootReducer = combineReducers({
-  coursersreducer, favsreducer, errorsreducer, usersreducer,
+  coursersreducer, favsreducer, errorsreducer,
 });
 
 export default rootReducer;
