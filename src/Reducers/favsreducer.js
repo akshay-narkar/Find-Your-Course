9369248b610f44c1a8595639bddb3b0a46cdf4e8
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-09 15:16:33 +0530

    Favs reducer trial code

--- src/Reducers/favsreducer.js
+++ src/Reducers/favsreducer.js
@@ -1,10 +1,12 @@
 import { FAVCOURSES, FAVCOURSEADD } from '../Actions/index';
 
 const favsreducer = (state = [], action) => {
+  // const statenew = { ...state };
   switch (action.type) {
     case FAVCOURSES: {
       const favs = [...state.favs, action.favs];
       return { ...state, favs }; }
+    // return statenew;}
     case FAVCOURSEADD: {
       const favlist = [...state.favlist, action.fav];
       return { ...state, favlist }; }
