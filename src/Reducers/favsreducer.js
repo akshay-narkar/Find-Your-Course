b3c0e4cccf9df9a0edb08ad0df1a37ae13307640
akshay-narkar <akshaynarkar1295@gmail.com>
2021-09-09 15:40:37 +0530

    Modify return statements

--- src/Reducers/favsreducer.js
+++ src/Reducers/favsreducer.js
@@ -1,12 +1,11 @@
 import { FAVCOURSES, FAVCOURSEADD } from '../Actions/index';
 
 const favsreducer = (state = [], action) => {
-  // const statenew = { ...state };
+  const statenew = { ...state };
   switch (action.type) {
-    case FAVCOURSES: {
-      const favs = [...state.favs, action.favs];
-      return { ...state, favs }; }
-    // return statenew;}
+    case FAVCOURSES:
+      statenew.favs = [action.favs];
+      return statenew;
     case FAVCOURSEADD: {
       const favlist = [...state.favlist, action.fav];
       return { ...state, favlist }; }
